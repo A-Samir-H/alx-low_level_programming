82476b5fc0925969c87accc4d77793e86ad8cc14
Ahmed Samir <plahmedsamir99@gmail.com>
2024-06-21 07:33:22 -0700

    Edit 5

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-	/*Using write function to print to stderr*/
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);/*59 is the length of the message*/
+	/*Print message to stderr*/
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
