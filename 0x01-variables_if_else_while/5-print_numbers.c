08df4b45aa67a74cef28c083b67fec8ba1ff2323
Ahmed Samir <plahmedsamir99@gmail.com>
2024-06-21 10:17:30 -0700

    First try for programe that print form 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i <= 9; i++)
+    {
+        printf("%d", i); /*Print each digit*/
+    }
+
+    printf("\n"); /*Print a new line at the end*/
+
+    return (0);
+}
