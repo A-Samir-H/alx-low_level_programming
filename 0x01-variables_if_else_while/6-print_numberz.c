544e16b682cef6232a67eba69abf82495014384c
Ahmed Samir <plahmedsamir99@gmail.com>
2024-06-21 10:25:28 -0700

    update errors

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i <= 9; i++)
-    {
-        putchar(i + '0'); /* Convert integer to corresponding ASCII digit */
-    }
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0'); /* Convert integer to corresponding ASCII digit */
+	}
 
-    putchar('\n'); /* Print a new line at the end */
+	putchar('\n'); /* Print a new line at the end */
 
-    return (0);
+	return (0);
 }
