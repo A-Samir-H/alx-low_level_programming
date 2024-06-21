dcad9d2e0d3f08cbaeef4d175dbf50464af78ffd
Ahmed Samir <plahmedsamir99@gmail.com>
2024-06-21 10:09:52 -0700

    Make program that print all alph and not print q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        /*Declare variable letter with value a*/
+        char letter = 'a';
+
+        while (letter <= 'z')
+        {
+		/*Make chect that letter not equal q and e*/
+		if (letter != 'q' && letter != 'e')
+			putchar(letter); /*print the current letter*/
+                letter++; /*Increment letter by 1 to go to the next letter in ASCII*/
+        }
+
+        /*output> new line character causing the next output to start on a new line.*/
+        putchar('\n');
+
+        return (0);
+}
