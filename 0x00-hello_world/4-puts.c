5c4a5f751da0a8b538483e753f4b0e28d7c106f0
Ahmed Samir <plahmedsamir99@gmail.com>
2024-06-21 06:26:45 -0700

    Write a C program that prints exactly print one sentence

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+*main - Puts Function to print only one sentence
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+		return 0;
+}
