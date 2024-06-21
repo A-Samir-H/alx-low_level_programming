9f305e3875a3ab25b1cd26664310439f50fa53bb
Ahmed Samir <plahmedsamir99@gmail.com>
2024-06-21 08:57:55 -0700

    Solve Errors in Spaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,13 +9,17 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
+
 	if(n > 0)
 		printf("%d is Positive\n", n); /*Print message if n is positive*/
 	else if(n < 0)
 		printf("%d is negative\n", n); /*Print message if n is negative*/
 	else
 		printf("%d is Zero\n", n); /*Print message if n is zero*/
+	
 	return (0);
 }
